8e0b4ba4b769ec667b54f82a9217c45d63d89ed3
srinidhi <42269243+srinidhimanjunath@users.noreply.github.com>
2020-02-03 22:25:54 +0530

    Create endUser.java
    u r a whiteboard wizard  responsible for designing  a software application that uses dragons for couriers and  passenger transport.
    there are different species of dragon for fulfil this purpose:
    1)The blue dragons can fly up to 100km/hr they are very small in size and can travel 2km/hr on land.
    2)The pink dragons:cannot fly they are twice the size of elephant which can accomdate 6 riders at a time they can do 10kms/hr a fully laden.
    3)the rainbow dragon:they are size of camel and can fly which can accomdate 1 rider in air and 2 riders on the ground they can fly at 50km/hr or run at 20km/hr on land
    
    design the hier of classes to model all this dragon species in a way they are useful for business

---
+++ endUser.java
@@ -0,0 +1,152 @@
+package rama;
+
+import java.util.Scanner;
+//import java.lang.*;
+
+class dragon{
+	
+	Scanner key = new Scanner(System.in);
+	private int rider;
+	private int rem;
+	private int dif;
+	//private int rem1;
+	boolean ch1;
+	//System.out.println("Enter the choice");
+	 void send()
+	{
+		System.out.println("Enter 1)courier and 2)Travel");
+		
+		int choice = key.nextInt();
+		switch(choice)
+		{
+		case 1:
+			blue_dragon obj1 = new blue_dragon();
+			obj1.send();
+		case 2:
+			System.out.println("Enter the total number of travelers");
+			rider = key.nextInt();
+			rem =rider / 6;
+			dif =   rider - 6*rem;
+			if(rem >= 1) {
+				pink_dragon obj2 = new pink_dragon();
+				obj2.send(rem);
+			}
+			if(dif >= 1)
+			{
+				rainbow_dragon obj3 = new rainbow_dragon();
+				obj3.send(dif);
+			}
+			
+			
+		}
+	
+	}
+	
+	boolean fly()
+	{
+		
+		System.out.println("Enter the whether you want use aerial transpotation with true or By a false");
+		ch1 = key.nextBoolean();
+		return ch1;
+	}
+	
+	
+	
+	
+	
+}
+
+
+class blue_dragon extends dragon{
+	private String Source;
+	private String  dest;
+	private int dist;
+	Scanner obj = new Scanner(System.in);
+	void send() {
+		System.out.println("Enter the source\n");
+		Source = obj.nextLine();
+		System.out.println("Enter the destination\n");
+		dest = obj.nextLine();
+		System.out.println("Enter the distance of travel approx\n");
+		dist = obj.nextInt();
+		if(super.fly() && dist>=20)
+		{
+			System.out.println("The courier has reached : " + dest +" From " + Source + " Through airways by blue dragon");
+		}
+		else if(super.fly() && dist>50)
+		{
+			System.out.println("You have chosen something that is not compatable plz restart the program make sure that you choose airways if the dist is greater than 50kms");
+			
+		}
+		else if(!super.fly())
+		{
+			System.out.println("The courier has reached : " + dest +" From " + Source + " Through landways by blue dragon");
+		}
+		
+	}
+	
+}
+
+class pink_dragon extends dragon{
+	 String Source;
+	 String  dest;
+	//private int dist;
+	Scanner obj = new Scanner(System.in);
+	void send(int ri) {
+		System.out.println("Enter the source\n");
+		Source = obj.nextLine();
+		System.out.println("Enter the destination\n");
+		dest = obj.nextLine();
+		//System.out.println("Enter the distance of travel approx\n");
+		//dist = obj.nextInt();
+		System.out.println( "With "+ ri + " dragons Travellers has travelled  from  " + Source +" to " + dest +" through lankways and using pink dragon.");
+		
+	}
+	
+}
+class rainbow_dragon extends dragon{
+	//private String Source;
+	//private String  dest;
+	//private int dist;
+	//Scanner obj = new Scanner(System.in);
+	pink_dragon ab = new pink_dragon();
+	void send(int ri) {
+		//System.out.println("Enter the source\n");
+		//Source = obj.nextLine();
+		//System.out.println("Enter the destination\n");
+		//dest = obj.nextLine();
+		//System.out.println("Enter the distance of travel approx\n");
+		//dist = obj.nextInt();
+		System.out.println("Plz Enter whetehr you want to use rainbow dragon in aerial mode or land mode by answering this simple question");
+		if(super.fly())
+		{
+			System.out.println(ri + " rainbow dragons are required to travel from " + ab.Source + " to " + ab.dest +" By landWays");
+		}
+		else if(!super.fly() )
+		{
+			System.out.println(ri%2 + " rainbow dragons are required to travel from " + ab.Source + " to " + ab.dest +" By airways");
+			
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+public class endUser {
+
+	
+	public static void main(String args[])
+	{
+		dragon d = new dragon();
+		d.send();
+		
+	}
+	
+	
+	
+}
